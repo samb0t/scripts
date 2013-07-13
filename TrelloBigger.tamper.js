d21de0ca35eee776aebf41bae2abe03b023fcef7
samb0t <samsnee123@yahoo.com>
2013-07-12 20:22:01 -0500

    Test commit trello script

---
+++ TrelloBigger.tamper.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name       TrelloBigger
+// @version    0.1
+// @description  this is awful, but wanted to test a commit
+// @match      https://trello.com/card/*
+// @copyright  2013+, sambot
+// @require http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+$(function () {
+    setTimeout(function () {
+        var width = $(window).width();
+        $(".window").css({width: width-100, left: '50px'}); 
+        $(".window-main-col").css({width: width-500});
+    }, 20);
+});
+
