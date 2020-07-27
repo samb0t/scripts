a0f14455553aad3b2d257b248979e66b6cc45dd5
samb0t <samsnee123@yahoo.com>
2020-07-26 22:03:56 -0500

    More backup dirs     refactored backup script     delete error file if empty

---
+++ MacScripts/mac-backup.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# Run this in crontab
+
+# errors on desktop
+# fix hidden files on target ntfs with --inplace
+# no -z (compression) to speed it up
+
+src_paths=(
+    "$HOME/./Calibre Library/"
+    "$HOME/./Documents/"
+    "$HOME/./Movies/"
+    "$HOME/./Music/"
+    "$HOME/./Pictures/"
+    "$HOME/./Splice/"
+
+    "$HOME/./Library/Pioneer/"
+    "$HOME/./Library/Audio/"
+    "$HOME/./Library/Application Support/Ableton/"
+    "$HOME/./Library/Application Support/Cycling '74/"
+    "$HOME/./Library/Application Support/Pioneer"
+
+    "/Library/Audio/"
+    "/Library/Application Support/Native Instruments/"
+)
+
+root_dest="/Volumes/media-backup/mac-backup/"
+error_file="$HOME/Desktop/$(date +%Y-%m-%d.txt)"
+
+for i in "${src_paths[@]}"
+do
+    dest=$root_dest
+
+    if  [[ $i == /Users* ]] ;
+    then
+        dest+="User/"
+    fi
+
+    rsync -avruR --inplace "$i" "$dest" 2> "$error_file"
+done
+
+if [ -f "$error_file" ] && [ ! -s "$error_file" ]
+then
+    rm "$error_file"
+fi
--- mac-backup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-#error on desktop
-rsync -avrzuR ~/./Documents/dev/ /Volumes/media-backup/mac-backup/ 2> "$(date +%Y-%m-%d_%H-%M-%S.txt)"
